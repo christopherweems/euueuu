28fbc7e129befd83d2fe6b791e3c21cdd30d70b1
Christopher Weems <hello@christopherweems.com>
2020-11-29 16:19:41 -0500

    Add `-c` flag to copy generated UUID to pasteboard

--- Sources/euueuu/main.swift
+++ Sources/euueuu/main.swift
@@ -1,11 +1,28 @@
+import AppKit
 import ArgumentParser
 import Foundation
 import unstandard
 
 struct UUIDGenerator: OutputResultParsableCommand {
+    @Flag(name: .shortAndLong, help: "Copy result to pasteboard")
+    var copyToPasteboard = false
+    
     mutating func run() throws {
+        let copyToPasteboard = self.copyToPasteboard
+        
         UUID().formatted()
         
+        ResultHandler { lines in
+            let pasteboard = Pasteboard()
+            assert(lines.count == 1)
+            
+            if copyToPasteboard, let line = lines.first {
+                pasteboard.clearContents()
+                pasteboard.setString(line)
+                
+            }
+            
+        }
     }
     
 }
